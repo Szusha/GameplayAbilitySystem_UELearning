362d3071786714ade85722fa37594ff53195bf40
akaliadz <01167187@pw.edu.pl>
2024-10-02 18:54:18 +0200

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Coryright Szusha
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; // How often the server will try to update Clients
+}
