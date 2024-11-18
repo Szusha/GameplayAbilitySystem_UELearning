91ae66a7e5e18b2b6fa032ab0fcd7a894374cb73
akaliadz <01167187@pw.edu.pl>
2024-11-18 18:52:26 +0100

    Broadcasting Effect Asset Tags

--- Source/Aura/Private/UI/WidgetControllers/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetControllers/OverlayWidgetController.cpp
@@ -33,6 +33,17 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 		AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
+		[](const FGameplayTagContainer& AssetTags)
+		{
+			for (const FGameplayTag& Tag : AssetTags)
+			{
+				const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Msg);
+			}
+		}
+	);
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
