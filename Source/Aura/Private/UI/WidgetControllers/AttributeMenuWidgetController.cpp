eff2e0339811f50fcd993d5da5fe4ee188bea8ca
akaliadz <01167187@pw.edu.pl>
2024-12-05 19:10:23 +0100

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Coryright Szusha
+
+
+#include "UI/WidgetControllers/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
---
+++ Source/Aura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+// Coryright Szusha
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+};
