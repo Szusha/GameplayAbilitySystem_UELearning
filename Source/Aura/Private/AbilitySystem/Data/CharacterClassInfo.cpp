d190591b3ce8ba4ce2385da16bcbeefed134a067
akaliadz <01167187@pw.edu.pl>
2024-12-22 11:36:55 +0100

    Character Class Info & Default Attributes Effects

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Coryright Szusha
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
