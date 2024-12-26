c5b60a006cfcd0359fab206b6f15efc6fe0a437a
akaliadz <01167187@pw.edu.pl>
2024-12-26 22:00:48 +0100

    Damage Execution Calculation

---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,21 @@
+// Coryright Szusha
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	UExecCalc_Damage();
+
+	virtual void Execute_Implementation(FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+	
+};
