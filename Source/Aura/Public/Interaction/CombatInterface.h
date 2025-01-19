efa6c43cc8944578a17dd87471e95d99962376fc
akaliadz <01167187@pw.edu.pl>
2025-01-19 10:24:00 +0100

    Elementalist Attack Task

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -75,6 +75,9 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	int32 GetMinionCount();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void IncrementMinionCount(int32 Amount);
 };
 
 
