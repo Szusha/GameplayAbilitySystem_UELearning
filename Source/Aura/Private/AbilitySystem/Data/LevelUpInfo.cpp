2a70b0ebdcbf221cdc60ec0619834c9df2abcfd4
akaliadz <01167187@pw.edu.pl>
2025-02-02 10:01:24 +0100

    Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,26 @@
+// Copyright Szusha
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// LevelUpInfo[1] = Level 1 Info
+		// LevelUpInfo[2] = level 2 Info
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
