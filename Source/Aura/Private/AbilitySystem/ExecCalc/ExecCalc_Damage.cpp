b01cb2003e16dbafa3993e3c2f72a648f90e07d0
akaliadz <01167187@pw.edu.pl>
2025-04-03 21:36:38 +0200

    Tying Radial Damage All Together

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -123,6 +123,10 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 		const FGameplayEffectAttributeCaptureDefinition CaptureDef = TagsToCaptureDefs[ResistanceTag];
 
 		float DamageTypeValue = EffectSpec.GetSetByCallerMagnitude(Pair.Key, false);
+		if (DamageTypeValue <= 0.f)
+		{
+			continue;
+		}
 
 		float Resistance = 0.f;
 		ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(CaptureDef, EvaluationParams, Resistance);
